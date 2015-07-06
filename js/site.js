6b51e3b0d2efcd81368ec9407b9d846ae353e7cd
Shawn Allen <shawn.allen@gsa.gov>
2015-07-06 15:51:43 -0700

    add un-minified select2.css

---
+++ js/site.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  $('select.select2').select2();
+
+  d3.selectAll('.picc-accordion')
+    .call(picc.accordion())
+    .on('open', function() {
+      var af = this.querySelector('[autofocus]');
+      if (af) af.focus();
+    });
+
+});
