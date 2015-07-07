e024eecb1369a69a541d3a0028beb0d644210a4a
Shawn Allen <shawn.allen@gsa.gov>
2015-07-07 15:29:56 -0700

    don't close select2 elements on selection

--- js/site.js
+++ js/site.js
@@ -1,6 +1,8 @@
 $(function() {
 
-  $('select.select2').select2();
+  $('select.select2').select2({
+    closeOnSelect: false
+  });
 
   d3.selectAll('.picc-accordion')
     .call(picc.accordion())
